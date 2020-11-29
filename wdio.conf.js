bcad5590c944802d2b86961defbd4f49bcff1446
Tanapon Kongjaroensuk <tanapon395@gmail.com>
2020-11-29 16:26:46 +0700

    update

--- wdio.conf.js
+++ wdio.conf.js
@@ -52,7 +52,9 @@ exports.config = {
       maxInstances: 5,
       //
       browserName: "chrome",
-      acceptInsecureCerts: true,
+      "goog:chromeOptions": {
+        args: ["headless", "disable-gpu"],
+      },
       // If outputDir is provided WebdriverIO can capture driver session logs
       // it is possible to configure which logTypes to include/exclude.
       // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
