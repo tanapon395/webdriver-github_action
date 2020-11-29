a19a7a7f185d7b715d0e138b6b6339bee0ad849a
Tanapon Kongjaroensuk <tanapon395@gmail.com>
2020-11-29 16:55:20 +0700

    update

--- test/specs/katalon.js
+++ test/specs/katalon.js
@@ -1,38 +1,38 @@
-var assert = require('assert');
+var assert = require("assert");
 
-describe('Automated Test Case', () =>  {
+describe("Automated Test Case", () => {
+  it("Search Google Chrome", async () => {
+    await browser.url("https://www.google.com/");
+    const keyword = await $('[name="q"]');
+    await keyword.click();
 
-	it('Search Google Chrome', async () => {
-        await browser.url('https://www.google.com/');
-        const keyword = await $('[name="q"]') 
-        await keyword.click()
+    await keyword.setValue("software engineering");
 
-        await keyword.setValue('software engineering');
+    const search = await $("(//input[@name='btnK'])[2]");
+    await search.click();
 
-        const search = await $('(//input[@name=\'btnK\'])[2]');
-        await search.click();
+    const link = await $('//div[@id="rso"]/div/div/div/a/h3/span');
+    await link.click();
 
-        const link = await $('//div[@id="rso"]/div/div/div/a/h3/span');
-        await link.click();
-
-        await expect(browser).toHaveTitle('เรียน Software Engineering แล้ว Coding ไม่ได้ ถือว่าไม่ประสบความสำเร็จในการเรียนจริงหรือ ? | by Yok • Rawisara | Artisan Digital | Medium');
-	});
-
+    await expect(browser).toHaveTitle(
+      "เรียน Software Engineering แล้ว Coding ไม่ได้ ถือว่าไม่ประสบความสำเร็จในการเรียนจริงหรือ ? | by Yok • Rawisara | Artisan Digital | Medium"
+    );
+  });
 });
 
 // From Katalon Export
 
-// var assert = require('assert');
+// var assert = require("assert");
 
-// describe('Untitled Test Case', function() {
-
-// 	it('should do something', function() {
-// 		browser.url('https://www.google.com/');
-// 		$('[name="q"]').click();
-// 		$('[name="q"]').setValue('software engineering');
-// 		$('(//input[@name=\'btnK\'])[2]').click();
-// 		$('//div[@id="rso"]/div/div/div/a/h3/span').click();
-// 		expect( $('#403c')).toHaveTextContaining(`เรียน Software Engineering แล้ว Coding ไม่ได้ ถือว่าไม่ประสบความสำเร็จในการเรียนจริงหรือ ?`);
-// 	});
-
+// describe("Untitled Test Case", function () {
+//   it("should do something", function () {
+//     browser.url("https://www.google.com/");
+//     $('[name="q"]').click();
+//     $('[name="q"]').setValue("software engineering");
+//     $("(//input[@name='btnK'])[2]").click();
+//     $('//div[@id="rso"]/div/div/div/a/h3/span').click();
+//     expect($("#403c")).toHaveTextContaining(
+//       `เรียน Software Engineering แล้ว Coding ไม่ได้ ถือว่าไม่ประสบความสำเร็จในการเรียนจริงหรือ ?`
+//     );
+//   });
 // });
